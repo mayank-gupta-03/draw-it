4b4a7b125dd477cf0d1a8e41227ac090081337a0
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2025-06-21 15:53:49 +0530

    Initialize user routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import userRoutes from "./routes/user.route";
+import { API_BASE_URL } from "@repo/common/config";
 
 const app = express();
 app.use(cors({ origin: process.env.CORS_ORIGIN || "", credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(`${API_BASE_URL}/users`, userRoutes);
 
 const PORT = process.env.PORT || 8000;
 
