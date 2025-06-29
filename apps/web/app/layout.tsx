005d560caaa4ec6e4fd3053b47fad35ca906a1c3
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2025-06-29 13:41:04 +0530

    Reinit web

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Providers } from "../components";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Providers>{children}</Providers>
+        {children}
       </body>
     </html>
   );
