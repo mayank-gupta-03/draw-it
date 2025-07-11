0a7d90bcc6e894b2f7492a046375b5b095ac9087
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2025-07-11 11:49:18 +0530

    Add WS_URL to config

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL = "/api/v1";
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const HOST_URL = "http://localhost:4000";
+export const WS_URL = "ws://localhost:4001";
